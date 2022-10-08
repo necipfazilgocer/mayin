f71ead5f6939d3548b094d159bc94f3e1d746ba7
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-10-09 00:39:49 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String args[]) {
+        MineSweeper mineSweeper = new MineSweeper();
+              int a=4;
+        System.out.println((a>>1)+(a<<2));
+        mineSweeper.Run();
+    
+    
+    }
+    
+}
